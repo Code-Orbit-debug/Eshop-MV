cd7503bc420b538829e14cfb0f88136afbb859be
Codingwithriha <rihashehzadi2003@gmail.com>
2026-06-11 10:51:11 +0500

    Initialise repo

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center text-3xl font-bold text-red-600">
+      Tailwind is working 🚀
+    </div>
+  );
+}
+
+export default App;
