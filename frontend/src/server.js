969e27043992ab3ff32ceebd17cae21be3d8b797
Codingwithriha <rihashehzadi2003@gmail.com>
2026-07-05 11:16:42 +0500

    fix: update backend API URLs in README and frontend server configuration

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,6 +1,6 @@
 export const server =
   process.env.REACT_APP_BACKEND_URL ||
-  "https://eshop-mv-production.up.railway.app/api/v2";
+  "https://eshop-mv-backend.vercel.app/api/v2";
 export const backend_url =
   process.env.REACT_APP_API_URL ||
-  "https://eshop-mv-production.up.railway.app";
+  "https://eshop-mv-backend.vercel.app";
